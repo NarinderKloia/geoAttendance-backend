ef9441b923ac034baa3445264c67d301357e88ee
Narinder Kloia <navirajput5566@gmail.com>
2026-07-28 08:38:06 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/geoattendance/GeoattendanceApplication.java
@@ -0,0 +1,13 @@
+package com.geoattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoattendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoattendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geoattendance/GeoattendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geoattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoattendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
